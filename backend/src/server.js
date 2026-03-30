ff739007286ff8544576113356bdab78c1af85f2
saad-nazir-0289 <l1s23bscs0289@ucp.edu.pk>
2026-03-30 18:20:45 +0500

    deployement thingy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,21 +4,35 @@ import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path from "path"
 
 dotenv.config();
 const app = express();
 
 //middleware
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }))
+}
 
 
 //routes
 app.use("/api/notes", rateLimiter, notesRoutes);
 
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
+    })
+}
 const PORT = process.env.PORT || 3000;
+
+
 
 //connect to database and then listen or start app
 connectDB().then(() => {
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
-
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3001/api" : "/api"
 const api = axios.create({
-    baseURL: "http://localhost:3001/api",
+    baseURL: BASE_URL,
     timeout: 10000,
 })
 
